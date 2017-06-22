2b9cbc930d1d2b5f70a09e58cae5d8cc27adf2a4
Diego Vilella <divilella@gmail.com>
2017-06-22 03:32:47 -0300

    Cadastro de denuncia ajustes
    Falta apenas implementar o registrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { FirebaseProvider } from '../providers/firebase-provider';
 import { MyApp } from './app.component';
 import { Camera } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
 
-import { DBMeter } from '@ionic-native/db-meter';
 
-//import { ProgressbarModule } from 'ngx-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -18,20 +18,16 @@ import { DBMeter } from '@ionic-native/db-meter';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
-    //ProgressbarModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp
   ],
   providers: [
-<<<<<<< HEAD
-    DBMeter,
     FirebaseProvider,
-=======
       Camera,
+      Geolocation,
       FirebaseProvider,
->>>>>>> b18834fe3d98fd53727ea3ce16d1ca7e1018e8d9
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/cad-denuncia/cad-denuncia.ts
+++ src/pages/cad-denuncia/cad-denuncia.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, AlertController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
+declare var google: any;
 
 @IonicPage()
 @Component({
@@ -8,15 +10,34 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
   templateUrl: 'cad-denuncia.html',
 })
 export class CadDenuncia {
+	//referente a localizacao
+	public mapa;
+	public latitude;
+	public longitude;
+	public coordenadas;
+	public endereco = "";
+	public i = 0;
+
+
+	//referente a foto
+	captureDataUrl: string;	
+
 	public options: CameraOptions = {
-		quality: 95,
+		quality: 50,
 		destinationType: this.camera.DestinationType.DATA_URL,
 		encodingType: this.camera.EncodingType.JPEG,
 		mediaType: this.camera.MediaType.PICTURE
 	};
 	public imagem;		
- 	constructor(public camera: Camera) {}
+ 	constructor(public camera: Camera, public geolocation: Geolocation, public alertCtrl: AlertController) {
+ 		this.alertCtrl = alertCtrl;
+ 		this.localizar();
 
+ 	}
+
+
+ 	// Funcoes referente a camera e imagem 
+//------------------------------------------------------------------------------------------
  	capturar() {	
 		this.camera.getPicture(this.options).then(
 			(imageData) => this.imagem = 'data:image/jpeg;base64,' + imageData,
@@ -24,8 +45,96 @@ export class CadDenuncia {
 		);
 	}
 
-	registrar() {
+	
 
+	registrar() {
+		//tem que criar a rotina ainda
+	}
+
+
+	upload() {
+	    let storageRef = firebase.storage().ref();
+
+	    const filename = Math.floor(Date.now() / 1000);
+
+	    const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+	    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL).then((snapshot)=> {
+	      this.Alert();
+    });
+
+  }
+
+  Alert() {
+    let alert = this.alertCtrl.create({
+      title: 'Uploaded!',
+      subTitle: 'Foto salva no Firebase',
+      buttons: ['OK']
+    });
+    alert.present();
+
+    this.captureDataUrl = "";
+  }
+
+  // Funcoes referente Localizacao
+//------------------------------------------------------------------------------------------
+
+ 	minhaLocalizacao() {
+		this.geolocation.getCurrentPosition().then(
+			position => {
+				this.latitude = position.coords.latitude;
+				this.longitude = position.coords.longitude;
+
+			}		
+		).catch(error => console.log(error));
+	}
+
+	abrirMapa() {
+		this.coordenadas = new google.maps.LatLng(this.latitude, this.longitude);
+
+		this.mapa = new google.maps.Map(document.getElementById('mapaView'), {
+			center: this.coordenadas,
+			zoom: 17,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			disableDefaultUI: true
+		});	
+	}
+
+	localizar() {
+		this.geolocation.getCurrentPosition().then(
+			position => {
+				this.latitude = position.coords.latitude;
+				this.longitude = position.coords.longitude;
+				this.abrirMapa();
+
+			}		
+		).catch(error => console.log(error));
+
+		
+	}
+
+	minhaPosicao() {
+		let marker = new google.maps.Marker({
+			map: this.mapa,
+			animation: google.maps.Animation.DROP,
+			position: this.coordenadas
+		})
+		this.informacoes();
+		marker.addListener('click', () => {
+			this.informacoes();
+		});
+	}
+
+
+	informacoes() {
+		this.i++;
+		let geo = new google.maps.Geocoder;
+		geo.geocode({ location: this.coordenadas }, (res, status) => {
+			if (status == google.maps.GeocoderStatus.OK) {
+				this.endereco = res[0].formatted_address;
+				this.endereco += " [" + this.i + "]"
+			}
+		});
 	}
   
 
