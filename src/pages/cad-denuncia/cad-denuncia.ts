1cbd908f5ac7b2c7ddf08a7d74506207df07dff4
brunoobatista <brunorafael.o@hotmail.com>
2017-06-22 21:58:08 -0300

    Linhas a mais

--- src/pages/cad-denuncia/cad-denuncia.ts
+++ src/pages/cad-denuncia/cad-denuncia.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-=======
-import { IonicPage } from 'ionic-angular';
->>>>>>> 4e1b60b1e06ed7a6b9f3d0db2f9aff26f03f1eef
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @IonicPage()
@@ -18,23 +14,18 @@ export class CadDenuncia {
 		encodingType: this.camera.EncodingType.JPEG,
 		mediaType: this.camera.MediaType.PICTURE
 	};
-	public imagem;		
+	public imagem;
  	constructor(public camera: Camera) {}
 
- 	capturar() {	
+ 	capturar() {
 		this.camera.getPicture(this.options).then(
 			(imageData) => this.imagem = 'data:image/jpeg;base64,' + imageData,
 			(err) => console.log(err)
 		);
 	}
 
-<<<<<<< HEAD
-
-=======
 	registrar() {
 
 	}
-  
->>>>>>> 4e1b60b1e06ed7a6b9f3d0db2f9aff26f03f1eef
 
 }
